cfbffcab5175d7d65c2a7f3b3b27ef1982e5f40c
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2011-12-03 18:57:10 +0200

    Repaired unicode issues.

--- basicbro/logger.py
+++ basicbro/logger.py
@@ -8,18 +8,19 @@ Logger module. Logs errors and information.
 
 
 import time
+import useful
 
 __all__ = ['Logger']
 
 def printbold(s): # prints text in bold
 	if len(s) > 0: # do not do anything if the string is empty
-		if s[-1] == '\n': s = s[:-1] # remove lineterminator if one
-		print('\033[0;1m'+str(s)+'\033[m')
+		if s[-1] == u'\n': s = s[:-1] # remove lineterminator if one
+		print('\033[0;1m%s\033[m'% s)
 
 def printred(s): # prints text in red
 	if len(s) > 0: # don't do anythin if string is empty
-		if s[-1] == '\n': s = s[:-1] # remove lineterminator if one
-		print('\033[0;31m'+str(s)+'\033[m')
+		if s[-1] == u'\n': s = s[:-1] # remove lineterminator if one
+		print('\033[0;31m%s\033[m'% s)
 
 class Logger(object):
     """
@@ -40,13 +41,15 @@ class Logger(object):
         """
         Single lines are logged with this
         """
-        
         try:
-            line = str(line).encode(self.encoding)
-            
+            if not isinstance(line, unicode):
+                line = str(line)
+            else:
+                line = line.encode(self.encoding)
+                
         except UnicodeEncodeError:
             printred('Logger.info() was unable to encode(%s) a line:'% self.encoding)
-            printred(line)
+            print(repr(line))
             return
                             
         timestamp = time.strftime('[%d.%m.%Y %H:%M:%S]')
@@ -74,11 +77,16 @@ class Logger(object):
         """
         
         try:
-            lines = map(lambda x: str(x).encode(self.encoding), lines)
+            for line in lines:
+                if not isinstance(line, unicode):
+                    line = str(line)
+                else:
+                    line = line.encode(self.encoding)
             
         except UnicodeEncodeError:
-            printred('Logger.error() was unable to encode(%s) a line:'% self.encoding)
-            printred('\n'.join(lines))
+            printred('Logger.error() was unable to encode(%s) a lines:'% self.encoding)
+            for line in lines:
+                print(repr(line))
             return
                         
         timestamp = time.strftime('[%d.%m.%Y %H:%M:%S]')
