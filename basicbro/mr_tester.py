32ddd0d09db7ec1578c8dc3793c14af50934af95
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2011-12-03 19:11:30 +0200

    Fixed mr_tester

--- basicbro/mr_tester.py
+++ basicbro/mr_tester.py
@@ -14,7 +14,7 @@ a.connect(('127.0.0.1', 48827))
 a.send('auth password\r\n')
 
 # Test utf-8 and line splitting
-a.send('webmsg #channel sender 'UTF-8 TEST: Č Ě Ď Ň Ř Ů Ĺ, Also split line test: '+ 'a'*120 + '012345678901234567890' + '\r\n')
+a.send('webmsg #channel sender UTF-8 TEST: Č Ě Ď Ň Ř Ů Ĺ, Also split line test: '+ 'a'*120 + '012345678901234567890' + '\r\n')
 
 # Test nicklisting for #channel
 a.send('nicklist #channel')
